a04074dbbf0043823f4f1bcbb140918e52728481
somnath711 <somnath.deoghriya@gmail.com>
2017-07-07 16:43:33 +0530

    footer updated for git link

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -44,8 +44,8 @@
             <div className='col-sm-5'>
               <h3 className='lead'><strong>Information</strong> and <strong>Copyright</strong></h3>
               <p>Powered by <strong>Node.js</strong>, <strong>MongoDB</strong> and <strong>React</strong> with Flux architecture and server-side rendering.</p>
-              <p>You may view the <a href='https://github.com/sahat/newedenfaces-react'>Source Code</a> behind this project on GitHub.</p>
-              <p>© 2015 Sahat Yalkabov.</p>
+              <p>You may view the <a href='https://github.com/somnath711/newedenfaces'>Source Code</a> behind this project on GitHub.</p>
+              <p>© 2017 Somnath Deoghriya.</p>
             </div>
             <div className='col-sm-7 hidden-xs'>
               <h3 className='lead'><strong>Leaderboard</strong> Top 5 Characters</h3>
