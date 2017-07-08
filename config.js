25b63b70730ef5bba5805c899aa288c82a5ad42e
somnath711 <somnath.deoghriya@gmail.com>
2017-07-08 18:19:59 +0530

    Update config.js
    Updated Config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  database: process.env.MONGO_URI || 'mongodb://somnathdeo:park3rpyne@ds151202.mlab.com:51202/nef'
+  database: process.env.MONGO_URI || 'mongodb://localhost/nef'
 };
